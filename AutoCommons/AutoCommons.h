92793168abde8e7567af0baf47fd9e314140f58d
Dhiya Ulhaq Zulha Alamsyah <dhiya@mamiteam.com>
2020-08-10 18:38:33 +0700

    Initial Commit

---
+++ AutoCommons/AutoCommons.h
@@ -0,0 +1,19 @@
+//
+//  AutoCommons.h
+//  AutoCommons
+//
+//  Created by Dhiya Ulhaq Zulha Alamsyah on 10/08/20.
+//  Copyright © 2020 Idolobi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutoCommons.
+FOUNDATION_EXPORT double AutoCommonsVersionNumber;
+
+//! Project version string for AutoCommons.
+FOUNDATION_EXPORT const unsigned char AutoCommonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoCommons/PublicHeader.h>
+
+
