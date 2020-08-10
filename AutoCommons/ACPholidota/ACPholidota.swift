1f1e81e16b2e9c14a2faee1b913deb84da66be86
Dhiya Ulhaq Zulha Alamsyah <dhiya@mamiteam.com>
2020-08-10 19:24:58 +0700

    Add Pholidota

---
+++ AutoCommons/ACPholidota/ACPholidota.swift
@@ -0,0 +1,76 @@
+//
+//  ACPholidota.swift
+//  AutoCommons
+//
+//  Created by Dhiya Ulhaq Zulha Alamsyah on 07/08/20.
+//  Copyright © 2020 Idolobi. All rights reserved.
+//
+
+import UIKit
+
+open class ACPholidota {
+    
+    private var containerView = UIView()
+    private var progressView = UIView()
+    private var activityIndicator = UIActivityIndicatorView()
+    
+    private static let shared = ACPholidota()
+    
+    public static func show(isShow: Bool, viewIfNeeded: UIView? = nil) {
+        if isShow {
+            shared.showProgressView(view: viewIfNeeded)
+        } else {
+            shared.hideProgressView()
+        }
+    }
+    
+    private func showProgressView(view: UIView? = nil) {
+        if let view = view {
+            containerView.frame = view.frame
+            containerView.center = view.center
+            
+            progressView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+            progressView.center = view.center
+        } else {
+            let window = UIWindow(frame: UIScreen.main.bounds)
+            containerView.frame = window.frame
+            containerView.center = window.center
+            
+            progressView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+            progressView.center = window.center
+        }
+        
+        containerView.backgroundColor = UIColor(hex: 0xffffff, alpha: 0.3)
+        
+        progressView.backgroundColor = UIColor(hex: 0x444444, alpha: 0.7)
+        progressView.clipsToBounds = true
+        progressView.layer.cornerRadius = 10
+        
+        activityIndicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        activityIndicator.style = .whiteLarge
+        activityIndicator.center = CGPoint(x: progressView.bounds.width / 2, y: progressView.bounds.height / 2)
+        
+        progressView.addSubview(activityIndicator)
+        containerView.addSubview(progressView)
+        UIApplication.shared.keyWindow?.addSubview(containerView)
+        
+        activityIndicator.startAnimating()
+    }
+    
+    private func hideProgressView() {
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+            self.containerView.removeFromSuperview()
+        }
+    }
+}
+
+fileprivate extension UIColor {
+    convenience init(hex: UInt32, alpha: CGFloat) {
+        let red = CGFloat((hex & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((hex & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(hex & 0xFF)/256.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
