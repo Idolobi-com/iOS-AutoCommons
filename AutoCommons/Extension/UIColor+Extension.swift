6665b7e4b1aae55ba78af067f14685ce760bfc61
Dhiya Ulhaq Zulha Alamsyah <dhiya@mamiteam.com>
2020-08-11 18:07:06 +0700

    Move file private UIColor to public extension

--- AutoCommons/Extension/UIColor+Extension.swift
+++ AutoCommons/Extension/UIColor+Extension.swift
@@ -151,6 +151,14 @@ extension UIColor {
         )
     }
     
+    convenience init(hex: UInt32, alpha: CGFloat = 1.0) {
+        let red = CGFloat((hex & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((hex & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(hex & 0xFF)/256.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
     convenience init(hex: String, alpha: CGFloat = 1.0) {
         var hexString: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
         if hexString.hasPrefix("#") {
